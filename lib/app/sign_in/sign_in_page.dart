ae4e8ade7f52b049cba7f4e0fa05bfe8c98c32b1
Protoculos <mazdak2012@gmail.com>
2021-08-23 03:03:14 +0300

    Ended all buttons

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker/app/sign_in/social_sign_in_button.dart';
+import 'package:time_tracker/common_widgets/custom_elevated_button.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -34,13 +36,15 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 48.0),
-          SignInButton(
+          SocialSignInButton(
+              assetName: 'assets/images/google-logo.png',
               text: 'Sign in with Google',
               textColor: Colors.black87,
               color: Colors.white,
               onPressed: () {}),
           const SizedBox(height: 8.0),
-          SignInButton(
+          SocialSignInButton(
+              assetName: 'assets/images/facebook-logo.png',
               text: 'Sign in with Facebook',
               textColor: Colors.white,
               color: const Color(0xFF334D92),
