ddaf2a5f5c4d8a9d717889688329c2004df18fe6
Protoculos <mazdak2012@gmail.com>
2021-08-26 02:20:23 +0300

    Refactor landing flow. Remove lifting state

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,7 +4,7 @@ import 'package:time_tracker/app/home_page.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker/services/auth.dart';
 
-class LangingPage extends StatefulWidget {
+class LangingPage extends StatelessWidget {
   final AuthBase auth;
 
   const LangingPage({
@@ -13,39 +13,19 @@ class LangingPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _LangingPageState createState() => _LangingPageState();
-}
-
-class _LangingPageState extends State<LangingPage> {
-  User? _user;
-  @override
-  void initState() {
-    super.initState();
-    _updateUser(widget.auth.currentUser);
-  }
-
-  void _updateUser(User? user) {
-    setState(() {
-      _user = user;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: widget.auth.authStateChanges(),
+      stream: auth.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final User? user = snapshot.data;
           if (user == null) {
             return SignInPage(
-              auth: widget.auth,
-              onSignIn: _updateUser,
+              auth: auth,
             );
           }
           return HomePage(
-            auth: widget.auth,
-            onSignOut: () => _updateUser(null),
+            auth: auth,
           );
         }
         return const Scaffold(
