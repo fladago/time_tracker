c4e0c2a2e3004021b59dc927a69d387914814154
Protoculos <mazdak2012@gmail.com>
2021-08-26 00:45:22 +0300

    Added depency injection abstract class Auth to home, landing, sign, main pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/landing_page.dart';
+import 'package:time_tracker/services/auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const LangingPage(),
+      home: LangingPage(auth: Auth()),
     );
   }
 }
