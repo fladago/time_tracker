e0c074e9dd761382099f26ed932aaab6e2d4d685
Protoculos <mazdak2012@gmail.com>
2021-08-25 18:24:36 +0300

    Added void Function(User?) onSignIn to SignInPage

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 
@@ -9,8 +10,12 @@ class LangingPage extends StatefulWidget {
 }
 
 class _LangingPageState extends State<LangingPage> {
+  User? _user;
   @override
   Widget build(BuildContext context) {
-    return const SignInPage();
+    if (_user == null) {
+      return const SignInPage();
+    }
+    return Container();
   }
 }
